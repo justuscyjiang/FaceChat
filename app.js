2108255c5c19becf4568214d126d2dfceb53fd09
cyJiang@DESKTOP-4D7T9SK <b04502101@ntu.edu.tw>
2019-06-07 10:18:04 +0800

    1

--- app.js
+++ app.js
@@ -17,44 +17,44 @@ var app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-io.on('connection', async (socket) => {
+io.on('connection', async(socket) => {
 
-  console.log('a user connected');
-  const clients = await io.engine.clientsCount;
+    console.log('a user connected');
+    const clients = await io.engine.clientsCount;
 
-  const socketid = socket.id;
-  socketHander = new SocketHander();
-  socketHander.connect();
+    const socketid = socket.id;
+    socketHander = new SocketHander();
+    socketHander.connect();
 
-  const history = await socketHander.getMessages();
+    const history = await socketHander.getMessages();
 
-  io.to(socketid).emit('history', history);
-  io.to(socketid).emit('clients', {
-    clients: clients,
-  });
-
-  socket.on("disconnect", () => {
-    console.log("a user go out");
-    io.emit("clients", {
-      clients: clients - 1,
+    io.to(socketid).emit('history', history);
+    io.to(socketid).emit('clients', {
+        clients: clients,
     });
-  });
 
-  socket.on("message", (obj) => {
-    socketHander.storeMessages(obj);
-    io.emit("message", obj);
-  });
-
-  socket.on('clients', (obj) => {
-    io.emit("clients", {
-      clients: clients,
-      user: obj,
+    socket.on("disconnect", () => {
+        console.log("a user go out");
+        io.emit("clients", {
+            clients: clients - 1,
+        });
     });
-  });
+
+    socket.on("message", (obj) => {
+        socketHander.storeMessages(obj);
+        io.emit("message", obj);
+    });
+
+    socket.on('clients', (obj) => {
+        io.emit("clients", {
+            clients: clients,
+            user: obj,
+        });
+    });
 
 });
 
-server.listen(3001);
+server.listen(process.env.PORT || 3001);
 
 // app.use(function (req, res, next) {
 //   res.io = io;
@@ -70,38 +70,38 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 });
 
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
