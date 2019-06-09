e2da2b042603be7eaab39f1468d74f8fc4d69e1d
cyJiang@DESKTOP-4D7T9SK <b04502101@ntu.edu.tw>
2019-06-10 05:49:21 +0800

    1447

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ io.on('connection', async(socket) => {
 
     socketHander = new SocketHander(); // private messages
     socketHander.connect();
-    const history = await socketHander.getMessagesP(); // ++++
+    const history = await socketHander.getMessages(); // ++++
     io.to(socketid).emit('history', history);
 
     io.to(socketid).emit('clients', {
@@ -62,7 +62,7 @@ io.on('connection', async(socket) => {
     });
 
     socket.on("message", (obj) => {
-        socketHander.storeMessagesP(obj); // +++++
+        socketHander.storeMessages(obj); // +++++
         io.emit("message", obj);
     });
 
